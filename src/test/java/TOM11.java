9dcbadcc4d9f8402894773244cd26f04c8a1a35b
TamaraShy <alisashylko@gmail.com>
2020-04-27 13:38:03 +0300

    TOM-11

---
+++ src/test/java/TOM11.java
@@ -0,0 +1,21 @@
+public class TOM11
+package MyCustomTestListener;
+
+        import org.testng.ISuite;
+        import org.testng.ITestNGListener;
+
+public class MyCustomTestListener implements ITestNGListener {
+
+    @Override
+    public void onStart (ISuite suite) {
+        System.out.println("Start Suite name: " + suite.getName());
+    }
+
+    @Override
+    public void onFinished (ISuite suite) {
+        System.out.println("Finish Suite name: " + suite.getName());
+    }
+
+}
+{
+}
